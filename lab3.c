238c83db13f7873a1110088f87ddd077cccdd5e2
Lipki3 <73040453+Lipki3@users.noreply.github.com>
2022-05-29 12:35:37 +0300

    lab3

---
+++ lab3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+void func1(void);
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	printf("Неравенство:7n^3 + 81n^2 - 10^6 < 0\n");
+	func1();
+		return 0;
+}
+void func1(void)
+{
+	int n, max = 0;
+	float res = 0;
+	n = 0;
+	do	   // цикл вычисления
+	{
+		n++;
+		res = 7 * pow(n, 3) + 81 * pow(n, 2) - pow(10, 6);
+		if (res < 0)
+			max = n;
+	} while (res < 0);
+	printf("Наибольшее целое решение неравенства: n = %d", max);
+}
